ff0fcab87095886a15613dc1ecca74415a58bc9c
Inga Zhamsoeva <injoyme@gmail.com>
2021-07-07 11:58:28 +0800

    make lint test

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,5 @@
 import readlineSync from 'readline-sync';
-console.log("Welcome to the Brain Games!");
+
+console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name? ');
-export { userName };
+export default userName;
