7fca8e4d799ed22748226c34576b267731842351
Inga Zhamsoeva <injoyme@gmail.com>
2021-07-24 01:26:57 +0800

    add calculate function to brain-calc

--- README.md
+++ README.md
@@ -6,14 +6,18 @@
 [![Test Eslint](https://github.com/inga888/frontend-project-lvl1/actions/workflows/eslint-test.yml/badge.svg)](https://github.com/inga888/frontend-project-lvl1/actions/workflows/eslint-test.yml)
 
 # Asciinemas #
-## To play the games type the commands listed bellow ##
+
 **brain-even**
 [![asciicast](https://asciinema.org/a/426902.svg)](https://asciinema.org/a/426902)
+
 **brain-calc**
 [![asciicast](https://asciinema.org/a/425275.svg)](https://asciinema.org/a/425275)
+
 **brain-gcd**
 [![asciicast](https://asciinema.org/a/425291.svg)](https://asciinema.org/a/425291)
+
 **brain-progression**
 [![asciicast](https://asciinema.org/a/425735.svg)](https://asciinema.org/a/425735)
+
 **brain-prime**
 [![asciicast](https://asciinema.org/a/425729.svg)](https://asciinema.org/a/425729)
--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -3,25 +3,26 @@ import playGame from '../index.js';
 
 const gameRule = 'What is the result of the expression?';
 
+const calculate = (operand1, operand2, operator) => {
+  switch (operator) {
+    case '+':
+      return operand1 + operand2;
+    case '-':
+      return operand1 - operand2;
+    case '*':
+      return operand1 * operand2;
+    default:
+      throw new Error('Sorry, not available operator');
+  }
+};
+
 const genGameData = () => {
   const num1 = getRandomInt(1, 100);
   const num2 = getRandomInt(1, 100);
   const operators = ['+', '-', '*'];
-  const randomOperator = operators[Math.floor(Math.random() * operators.length)];
-  const question = `Question: ${num1} ${randomOperator} ${num2}`;
-  let correctAnswer;
-  // eslint-disable-next-line default-case
-  switch (randomOperator) {
-    case '+':
-      correctAnswer = num1 + num2;
-      break;
-    case '-':
-      correctAnswer = num1 - num2;
-      break;
-    case '*':
-      correctAnswer = num1 * num2;
-      break;
-  }
+  const randomOperator = operators[getRandomInt(0, operators.length - 1)];
+  const question = `${num1} ${randomOperator} ${num2}`;
+  const correctAnswer = calculate(num1, num2, randomOperator);
   const result = [question, String(correctAnswer)];
   return result;
 };
